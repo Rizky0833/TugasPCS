d2be6c0d551f21d707203ebc8b9012f2256e1254
Rizky Permana <riizkypermana@gmail.com>
2020-12-12 16:30:09 +0700

    tugas pcs

---
+++ app/src/main/java/com/appmenu/app/ui/home/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.appmenu.app.ui.home
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.appmenu.app.R
+import com.appmenu.app.databinding.ActivityMainBinding
+import com.appmenu.app.ui.auth.AppmenuAuth
+import com.appmenu.app.ui.auth.AuthActivity
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.buttonLogout.setOnClickListener {
+            AppmenuAuth.logout(this) {
+                startActivity(Intent(this, AuthActivity::class.java))
+                finish()
+            }
+        }
+    }
+}
